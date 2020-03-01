36fbd93dcac870d866ca6b1f8a04fd84c2e1a7c5
Dandy <dandyjreyes@gmail.com>
2020-03-01 10:27:42 -0800

    added controller for merchandise

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -1,7 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.join(__dirname, '../db/thumbnail.json');
+
 exports.getData = async (request, response, next) => {
   try {
-    const allData = await 'this is all the data...';
-    response.send(allData);
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        response.send(err.message);
+      }
+      response.send(JSON.parse(data));
+    });
+    // const allData = await 'this is all the data...';
+    // // response.send(allData);
   } catch (err) {
     next(err);
   }
